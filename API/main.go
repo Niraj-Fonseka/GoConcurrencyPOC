b74f8f54f08470bd795b38746f432fb793f2095b
MNF4691 <m_n_fonseka@homedepot.com>
2018-03-05 19:18:13 -0600

    Added Gocache

--- API/main.go
+++ API/main.go
@@ -4,24 +4,58 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 	"time"
+
+	"github.com/patrickmn/go-cache"
 
 	"github.com/gorilla/mux"
 )
 
+var c = cache.New(1*time.Minute, 1*time.Minute)
+
 func main() {
 
 	router := mux.NewRouter()
+	// Set the value of the key "foo" to "bar", with the default expiration time
+	c.Set("foo", "bar", cache.DefaultExpiration)
+	c.Set("REIN", "reintoken", 30*time.Second)
 
+	fmt.Println("Running Listening")
 	router.HandleFunc("/health", GetHealth).Methods("GET")
 	router.HandleFunc("/handleFirst", GetFirst).Methods("GET")
 	router.HandleFunc("/handleSecond", GetSecond).Methods("GET")
 	router.HandleFunc("/handleThird", GetThird).Methods("GET")
 	router.HandleFunc("/handleFourth", GetFourth).Methods("GET")
+	router.HandleFunc("/cache", GetCacheData).Methods("GET")
 	router.HandleFunc("/handleFifth", GetFifth).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 
+}
+
+func GetCacheData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("In get cache data function")
+	foo, found := c.Get("REIN")
+	if !found {
+		fmt.Println("Expired Token  : Need to call auth and set the token")
+		GetReinAuth()
+	}
+	fmt.Printf("Found : %t , Value : %s \n", found, foo)
+}
+
+func GetReinAuth() string {
+	fmt.Println("Calling Rein Auth call")
+	c.Set("REIN", "reintoken", 30*time.Second)
+
+	token, _ := c.Get("REIN")
+	t := reflect.TypeOf(token)
+	for i := 0; i < t.NumField(); i++ {
+		fmt.Printf("%+v\n", t.Field(i))
+	}
+
+	return "reintoken"
+	//return token
 }
 
 func GetHealth(w http.ResponseWriter, r *http.Request) {
